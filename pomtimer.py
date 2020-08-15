a5110973e71e8a8a1366431417c2630ef797a973
Carl-Be <carlbechie@carlbechie.com>
2020-08-14 23:50:00 -0700

    added feature of showing defuse mode type (short or long breaK). Long break is every 4th defuse mode break.

--- pomtimer.py
+++ pomtimer.py
@@ -16,8 +16,11 @@ from pydub.playback import play
 from asciiArt import asciiArt
 
 
+
+
+
 # gets the time input from the user  
-def askForInput(logicalMode):
+def askForInput(logicalMode, breakType):
     
     if logicalMode == 1:
 
@@ -25,8 +28,12 @@ def askForInput(logicalMode):
         return time
 
     elif logicalMode == 0:
-        
-        time = input('\nDefuse Mode: Type 1-60 or 0 to exit\nHow many minutes: ')
+
+        if breakType == 0: # long break
+            time = input('\nDefuse Mode Long Break: Type 1-60 or 0 to exit\nHow many minutes: ')
+        elif breakType != 0: # short break 
+            time = input('\nDefuse Mode Short Break: Type 1-60 or 0 to exit\nHow many minutes: ')
+            
         return time
 
 # resets the timer after goal time is meet
@@ -39,22 +46,24 @@ def resetTimer(timeModeList):
     
     return 
 
-def printFoucus(timeModeList):
+def printFoucus(timeModeList, time):
 
     print(asciiArt(61) + asciiArt(timeModeList[1]))
-    sleep(1) # sleep program for 5 seconds  
+    sleep(time) # sleep program for 5 seconds  
 
     return
 
-def printDefuse(timeModeList): 
+def printDefuse(timeModeList, time): 
    
     print(asciiArt(62) + asciiArt(timeModeList[1]))
-    sleep(1) # sleep program for 5 seconds  
+    sleep(time) # sleep program for 5 seconds  
 
     return
     
 # display the timer 
 def displayController(timeModeList):
+    
+    time = 60 # seconds to pause screen  
 
     # finish message display 
     if timeModeList[1] == 0: # print the finish message  
@@ -62,12 +71,12 @@ def displayController(timeModeList):
 
     # focus mode discplay 
     if timeModeList[1] > 0 and timeModeList[0] % 2 == 1: 
-        printFoucus(timeModeList)
+        printFoucus(timeModeList, time)
         return
 
     # defuse mode display 
     elif timeModeList[1] > 0 and timeModeList[0] % 2 == 0:
-        printDefuse(timeModeList)
+        printDefuse(timeModeList, time)
         return
 
 # Function clears the screen before the next display
@@ -82,7 +91,9 @@ def clearScreen():
 def getMoreTime(modeCount):
 
     logicalMode = modeCount % 2 # if mode is odd its focus mode if mode count if even its defuse mode
-    time = askForInput(logicalMode)  
+    breakType = modeCount % 8 # If Remainder is 0 long break. Long break every 4th break.
+    time = askForInput(logicalMode, breakType) 
+    
     return time 
 
 # check to see if the time is 0 or not 
@@ -129,7 +140,7 @@ def checkIntialTime(modeCount, time):
 
 def main():
     modeCount = 1 # starts out in focus mode 
-    time = askForInput(modeCount) # get intial time 
+    time = askForInput(modeCount, None) # get intial time 
     checkIntialTime(modeCount, time)
 
 if __name__ == '__main__':
